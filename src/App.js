c4989860c12928e436f22027cb5b0d62daaec988
Zachary Weiland <zacharyweiland@Zacharys-MBP.attlocal.net>
2021-03-22 10:47:38 -0500

    My first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" dir="ltr">
+      <div>
+        <div tabindex="-1" class="_1i7ccu91">
+
+          <div class="_fambuz" aria-hidden="true">
+            <div class="_1gw6tte">
+              <div class="fixed-gallery">
+                <img alt="house 1" src="https://pi.movoto.com/p/404/1634652_0_vRnjiI_l.jpeg" />
+                <img alt="house 2" src="https://pi.movoto.com/p/404/1634652_0_JbvbqZ_l.jpeg" />
+                <img alt="house 3" src="https://pi.movoto.com/p/404/1634652_0_nmINRz_l.jpeg" />
+                <img alt="house 4" src="https://pi.movoto.com/p/404/1634652_0_Ne7izE_l.jpeg" />
+                <img alt="house 5" src="https://pi.movoto.com/p/404/1634652_0_VzQZEn_l.jpeg" />
+                <img alt="house 6" src="https://pi.movoto.com/p/404/1634652_0_R3ayRj_l.jpeg" />
+              </div>
+            </div>
+          </div>
+
+          <div class="_17slp5e">
+
+          </div>
+
+          <div class="_6bm0d7l">
+            <div class="_moiu1e">
+              <div class="_1i59r47o">
+                <div class="_1gw6tte">
+                  <div class="_1shuizm2">
+                  </div>
+                </div>
+              </div>
+
+              <div class="_y9ev9r" aria-hidden="false">
+                <div class="_36nbv2w">
+                  <div class="_diqct2z">
+                    <div class="home-summary">
+                      <div class="summary-meta">
+                        <span>House For Sale</span>  <span>Active · 3 Days</span>
+                      </div>
+                      <div class="summary-price">
+                        <strong class="home-price">$275,000</strong>
+                      </div>
+                      <div class="summary-address">
+                        <span>2424 Santa Anna, San Antonio, TX 78704</span>
+                      </div>
+                      <div class="summary-space">
+                        <span><strong>3</strong> Bd · <strong>2</strong> Ba · <strong>1,336</strong> Sqft · <strong>$194</strong>/Sqft</span>
+                      </div>
+                    </div>
+                    <div class="tabs">
+                      <ul class="unstyled">
+                        <li></li>
+                      </ul>
+                    </div>
+                    <div>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent iaculis aliquam diam eu lobortis. Proin vitae consectetur neque, at imperdiet est. Vestibulum at facilisis justo. Nullam lorem lacus, ultricies vel scelerisque fringilla, feugiat vel risus. Mauris sit amet risus sit amet nisl eleifend efficitur ac in massa. Vestibulum consectetur eu dolor vitae pretium. Nunc eu dignissim tortor. Morbi rutrum scelerisque facilisis. Proin id aliquet est. Proin tempus posuere fringilla.</p>
+                      <p>Donec eget facilisis urna, a imperdiet nulla. Sed cursus id nulla eu facilisis. In et nunc quis nisl accumsan accumsan. Integer commodo aliquam euismod. Aenean dapibus turpis eu lectus pulvinar aliquet.</p>
+                      <p>Sed vel porta tellus. Donec suscipit posuere nulla quis lacinia. Morbi condimentum dui quis eros maximus, fringilla tempus ante tincidunt. Etiam ac rhoncus orci. Etiam leo diam, eleifend id neque vel, convallis vestibulum risus. Praesent quis magna eget orci varius egestas. Donec accumsan ex a ante vulputate porttitor. Quisque sed quam mi. Sed ultrices nibh in hendrerit tincidunt. Quisque ullamcorper augue ligula, et aliquam quam mollis non. Nam a euismod erat, suscipit rhoncus sapien. Sed metus mi, rutrum fermentum volutpat eu, dictum in nulla.</p>
+                      <p>Ut maximus euismod neque tristique venenatis. Duis porttitor justo vel neque tincidunt, nec tempor libero congue. Duis ac auctor urna, sit amet commodo odio. Suspendisse suscipit justo in consectetur tincidunt. Aliquam sit amet libero odio. Quisque congue aliquet tellus, at aliquet nulla pretium ac. Fusce efficitur, justo ac fermentum sollicitudin, sapien turpis pretium leo, et sagittis ex velit et ipsum.</p>
+                      <p>Aenean lobortis sem id mattis gravida. Quisque leo massa, elementum non euismod vitae, facilisis et ex. Nunc at ex sit amet ipsum rhoncus vehicula. Aliquam ullamcorper faucibus consequat. Donec sed enim id nisl vulputate vestibulum et sit amet nisi. Praesent consectetur cursus molestie. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus egestas egestas. Phasellus in turpis lectus. Praesent in fringilla turpis, sit amet pharetra ante. Praesent metus felis, sollicitudin nec porttitor ac, aliquam ut massa. In eget hendrerit orci. Integer sit amet risus risus.</p>
+                      <p>Vestibulum velit leo, laoreet non bibendum id, venenatis in libero. Curabitur faucibus facilisis erat, vitae dapibus nisl sollicitudin non. Vivamus sed tempor dui. Nullam ac lectus id tortor facilisis suscipit. Nam dictum sagittis ante, maximus ultrices elit. Cras facilisis, orci pulvinar rhoncus mollis, est turpis consequat arcu, ac consectetur arcu dolor molestie ligula. Duis molestie risus lobortis purus vestibulum iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <nav aria-hidden="false" class="_1m294zs">
+      		<div class="_h0klba">
+      			<a aria-label="Explore" class="_1e1aumhl" href="/" type="button">
+      			<div class="_jro6t0">
+      				<div class="_jgit7bg">
+
+      				</div>
+      			</div>
+      			<div class="_cvn1xw5">
+      				Explore
+      			</div>
+            </a>
+            <a aria-label="Saved" class="_45o9ez" href="/wishlists" type="button">
+        			<div class="_jro6t0">
+        				<div class="_tbbnrnx">
+
+        				</div>
+        			</div>
+        			<div class="_1nudv8w">
+        				Saved
+        			</div>
+            </a>
+            <a aria-label="Log in" class="_45o9ez" data-veloute="pwa-tab-bar-item-login" href="/login" type="button">
+        			<div class="_jro6t0">
+        				<div class="_tbbnrnx">
+
+        				</div>
+        			</div>
+        			<div class="_1nudv8w">
+        				Log in
+        			</div>
+            </a>
+      		</div>
+        </nav>
+      </div>
     </div>
   );
 }
